1b8c5a84dbc91ab00fe93d7042711cfe714e204b
antoniolaikauf <antonio.laikauf00@gmail.com>
2023-12-13 13:51:52 +0100

    config: set-up per bonus

---
+++ bonus/JS/index.js
@@ -0,0 +1,19 @@
+const {createApp}= Vue;
+
+createApp({
+    data(){
+        return{
+            arrayEmail:[],
+        }
+    },
+    mounted(){
+        // ciclo for per crichiedere le email
+      for (let i = 0; i < 10; i++) {
+        // richiesta per prendere email
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((risposta)=>{
+         this.arrayEmail.push(risposta.data.response);
+        })
+      }
+    }
+}).mount("#app");
